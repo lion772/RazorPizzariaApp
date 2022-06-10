df907dd486ae279b47c75e9676e2db0ffc6f04f8
william <william.steinkedemello@gmail.com>
2022-06-10 12:38:27 +0200

    create OnGet function, get HTTP Post data and display it

--- RazorPizzaApp/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizzaApp/Pages/Checkout/Checkout.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzaApp.Models;
 
 namespace RazorPizzaApp.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        //[BindProperty]
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+
         }
     }
 }
